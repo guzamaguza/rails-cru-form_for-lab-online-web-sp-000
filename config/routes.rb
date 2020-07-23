6bfc5e19eb3b361f8c16c811a049d9f275894177
Mark A Guza <guzamaguza@gmail.com>
2020-07-23 03:11:06 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  get 'songs/name'
+
+  get 'songs/artist_id'
+
+  get 'songs/genre_id'
+
+  get 'genres/name'
+
+  get 'artists/name'
+
+  get 'artists/bio'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
